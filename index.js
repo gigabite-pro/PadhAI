1ff9811646be46506e1dee57f2cc65f3742da76a
Vaibhav Sharma <rajmachawal19@gmail.com>
2023-08-25 19:01:49 +0530

    ppt to jpg, frontend

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const {isAuthorized} = require('./config/authCheck');
+const bodyParser = require("body-parser");
+var convertapi = require('convertapi')('3CVROT6hAnNl3ri3');
 const authRoute = require('./routes/auth');
 require('dotenv').config();
 
 // Middlewares
+app.use(express.static(__dirname + '/public'));
 app.set('views', (__dirname + '/views'))
 app.set('view engine', 'ejs');
 app.use(
     session({
-        secret : 'yoyohoneysingh',
+        secret : process.env.SESSION_SECRET,
         cookie : {
             maxAge : 60000 * 60 * 24
         },
@@ -19,8 +22,10 @@ app.use(
         resave : false
     })
 )
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 
 // DB connection
 mongoose.set('strictQuery', true);
@@ -34,7 +39,34 @@ mongoose.connect(process.env.MONGO_URI,{
 app.use('/auth', authRoute);
 
 app.get('/', (req,res)=>{
-    res.send('Go to /auth/login')
+    res.render('index')
+})
+
+app.get('/upload', (req,res)=>{
+    res.render('upload')
+})
+
+
+app.post('/uploadFile', (req,res)=>{
+    const fileUrl = req.body.fileUrl
+    console.log(fileUrl)
+
+    convertapi.convert('jpg', { File: fileUrl })
+    .then(function(result) {
+      // get converted file url
+      result.files.forEach(file => {
+        console.log(file.url)
+      });
+    })
+    .catch(function(e) {
+      console.error(e.toString());
+    });
+
+    res.redirect('/uploaded')
+});
+
+app.get('/uploaded', async (req,res)=>{
+    res.send('uploaded')
 })
 
 const PORT = process.env.PORT || 3000;
--- routes/auth.js
+++ routes/auth.js
@@ -69,7 +69,7 @@ router.get('/callback', async (req, res)=>{
         if(doc){
             //login
             req.session.user = doc
-            res.redirect('/map')
+            res.redirect('/dashboard')
         }else{
             //register
             const newUser = new Users({
@@ -81,7 +81,7 @@ router.get('/callback', async (req, res)=>{
             newUser.save()
             .then((resp)=>{
                 req.session.user = resp
-                res.redirect('/map')
+                res.redirect('/dashboard')
             })
             .catch(err => console.log(err))
         }
