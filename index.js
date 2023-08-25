dd704972bd2ab128fa6255d9a84bedc2d08a1883
Vaibhav Sharma <rajmachawal19@gmail.com>
2023-08-26 04:06:16 +0530

    final

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ require('firebase/storage');
 const axios = require('axios');
 const authRoute = require('./routes/auth');
 const testRoute = require('./routes/test');
-const Users = require('./models/Users')
+const Users = require('./models/Users');
+const { url } = require('inspector');
 require('dotenv').config();
 
 // Middlewares
@@ -79,21 +80,35 @@ app.post('/uploadFile', (req,res)=>{
             const uid = req.session.user._id
             const user = await Users.findOne({_id: uid})
             if (user.personality == "auditory") {
+                var mainText = ''
+                resp.data.ParsedResults.forEach(text => {
+                    mainText = mainText + text.ParsedText.replace(/[\r\n]/gm, '')
+                })
                 const request = {
-                    input: {text: resp.data},
+                    input: {text: mainText},
                     // Select the language and SSML voice gender (optional)
                     voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
                     // select the type of audio encoding
                     audioConfig: {audioEncoding: 'MP3'},
                   };
             
-                  const [response] = await client.synthesizeSpeech(request);
-                  // Write the binary audio content to a local file
-                  const writeFile = util.promisify(fs.writeFile);
-                  await writeFile('output.mp3', response.audioContent, 'binary');
-                  console.log('Audio content written to file: output.mp3');
+                const [response] = await client.synthesizeSpeech(request);
+                // Write the binary audio content to a local file
+                const writeFile = util.promisify(fs.writeFile);
+                await writeFile('output.mp3', response.audioContent, 'binary');
+                console.log('Audio content written to file: output.mp3');
 
-                  const firebaseConfig = {
+                var admin = require("firebase-admin");
+
+                var serviceAccount = require("D:\\Giga-bite pro\\Codimg\\Eysa23\\firebase_creds.json");
+
+                admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount),
+                storageBucket: 'gs://esya23-3b426.appspot.com'
+                });
+
+
+                const firebaseConfig = {
                     apiKey: "AIzaSyDxwkYGFGLTeEOo7sDzXoQspEeEH1dWD9E",
                     authDomain: "esya23-3b426.firebaseapp.com",
                     projectId: "esya23-3b426",
@@ -105,24 +120,34 @@ app.post('/uploadFile', (req,res)=>{
 
                 firebase.initializeApp(firebaseConfig);
 
-                const storage = firebase.storage();
-                const storageRef = storage.ref();
-                const filePath = 'output.mp3';
-                const name = `${new Date}-${filePath}`
-                const remotePath = `uploads/${name}`; // Destination path in Firebase Storage
+                const storage = admin.storage();
+                const bucket = storage.bucket();
 
+                // Upload a file to Firebase Storage
                 async function uploadFile() {
-                try {
-                    const fileRef = storageRef.child(remotePath);
-                    const fileSnapshot = await fileRef.put(filePath);
+                    const filePath = 'output.mp3'; // Replace with the actual file path
+                    const fileName = `${new Date}-output.mp3`;
 
+                    await bucket.upload(filePath, {
+                        destination: fileName,
+                        metadata: {
+                            contentType: 'file/mp3' // Adjust content type as needed
+                        }
+                    }); 
+
+                    // Generate a download URL for the uploaded file
+                    const downloadUrl = await bucket.file(fileName).getSignedUrl({
+                        action: 'read',
+                        expires: '03-09-2030' // Set an expiration date for the URL
+                    });
+
+                    console.log('Download URL:', downloadUrl[0]);
                     console.log('File uploaded successfully.');
-                } catch (error) {
-                    console.error('Error uploading file:', error);
-                }
+                    res.render('audio', {url: downloadUrl[0]})
                 }
 
-                uploadFile();
+                uploadFile()
+                .catch(console.error);
 
             } else {
                 const prompt = `Provide a bar graph showing variation in GDP of 5 real Asian Countries. Give it in a format that can be sent to Quickchart API. Format the JSON such that it can be parsed.`
@@ -144,7 +169,7 @@ app.post('/uploadFile', (req,res)=>{
 
                     // Print the chart URL
                     console.log(myChart.getUrl());
-                    res.send('done')
+                    res.render('chart', {url: myChart.getUrl()})
                 })
             }
         }).catch(err => {
@@ -155,13 +180,7 @@ app.post('/uploadFile', (req,res)=>{
     .catch(function(e) {
       console.error(e.toString());
     });
-
-    res.redirect('/uploaded')
 });
-
-app.get('/uploaded', async (req,res)=>{
-    res.send('uploaded')
-})
 
 const client = new textToSpeech.TextToSpeechClient();
 
